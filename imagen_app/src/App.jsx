889ab335f94baf82faf43b63ed7ee901ec02d257
Louis Paulet <louispaulet13@gmail.com>
2025-05-05 19:57:51 +0200

    add dummy homepage

--- imagen_app/src/App.jsx
+++ imagen_app/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import About from './components/About';
+import DummyHomepage from './components/DummyHomepage';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow">
-        <About />
+        <DummyHomepage />
       </main>
       <Footer />
     </div>
