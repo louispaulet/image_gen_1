e74d344fc1bdb49a498a8d146c027a90bfa871ae
Louis Paulet <louispaulet13@gmail.com>
2025-05-05 23:46:08 +0200

    light refactor: rename dummy homepage filename

--- imagen_app/src/App.jsx
+++ imagen_app/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import DummyHomepage from './components/DummyHomepage';
+import DummyHomepage from './components/Homepage';
 import About from './components/About';
 
 function App() {
